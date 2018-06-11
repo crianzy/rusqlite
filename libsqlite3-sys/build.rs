820e083765eb557198f730e64313804d3e126676
wangsijie <wangsijie@bytedance.com>
2018-06-11 17:20:06 +0800

    feat: support bundled sqlcipher
    Change-Id: I9081c47a4abc78f13082c37f83faa7dd85aeec24

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -9,18 +9,20 @@ mod build {
     use std::path::Path;
 
     pub fn main() {
-        if cfg!(feature = "sqlcipher") {
-            panic!("Builds with bundled SQLCipher are not supported");
-        }
-
         let out_dir = env::var("OUT_DIR").unwrap();
         let out_path = Path::new(&out_dir).join("bindgen.rs");
-        fs::copy("sqlite3/bindgen_bundled_version.rs", out_path)
+
+        let bundled_file = if cfg!(feature = "sqlcipher") {
+            "sqlcipher/bundled_sqlcipher.rs"
+        } else {
+            "sqlite3/bindgen_bundled_version.rs"
+        };
+
+        fs::copy(bundled_file, out_path)
             .expect("Could not copy bindings to output directory");
 
         let mut cfg = cc::Build::new();
-        cfg.file("sqlite3/sqlite3.c")
-            .flag("-DSQLITE_CORE")
+        cfg.flag("-DSQLITE_CORE")
             .flag("-DSQLITE_DEFAULT_FOREIGN_KEYS=1")
             .flag("-DSQLITE_ENABLE_API_ARMOR")
             .flag("-DSQLITE_ENABLE_COLUMN_METADATA")
@@ -39,9 +41,58 @@ mod build {
             .flag("-DSQLITE_THREADSAFE=1")
             .flag("-DSQLITE_USE_URI")
             .flag("-DHAVE_USLEEP=1");
+        if cfg!(feature = "optimize") {
+            cfg.flag("-DSQLITE_OMIT_SHARED_CACHE")
+                .flag("-DSQLITE_DEFAULT_MEMSTATUS=0")
+                .flag("-DSQLITE_OMIT_PROGRESS_CALLBACK")
+                .flag("-DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1");
+        }
+
         if cfg!(feature = "unlock_notify") {
             cfg.flag("-DSQLITE_ENABLE_UNLOCK_NOTIFY");
         }
+
+        if cfg!(feature = "sqlcipher") {
+            cfg.file("sqlcipher/sqlite3.c")
+                .flag("-DSQLITE_HAS_CODEC")
+                .flag("-DSQLITE_TEMP_STORE=2");
+
+            let target = env::var("TARGET").unwrap();
+
+            // Default to CommonCrypto on Apple systems unless the "openssl" feature is explicitly
+            // given.
+            if target.contains("apple") && !cfg!(feature = "openssl") {
+                cfg.flag("-DSQLCIPHER_CRYPTO_CC");
+                println!("cargo:rustc-link-lib=framework=CoreFoundation");
+                println!("cargo:rustc-link-lib=framework=Security");
+            } else if cfg!(feature = "tomcrypt") {
+                cfg.flag("-DSQLCIPHER_CRYPTO_LIBTOMCRYPT");
+                println!("cargo:rustc-link-lib=tomcrypt");
+            } else {
+                cfg.flag("-DSQLCIPHER_CRYPTO_OPENSSL");
+
+                if let Some(dir) = env::var_os("OPENSSL_DIR") {
+                    let ssl_root = Path::new(&dir);
+                    cfg.flag(&format!("{}{}", "-I", ssl_root.join("include").to_str().unwrap()));
+                    println!("cargo:rustc-link-search={}", ssl_root.join("lib").to_str().unwrap());
+                    if target.contains("windows") {
+                        println!("cargo:rustc-link-lib={}={}", "static", "ssl");
+                        println!("cargo:rustc-link-lib={}={}", "static", "crypto");
+                        println!("cargo:rustc-link-lib=gdi32");
+                        if target.contains("msvc") {
+                            println!("cargo:rustc-link-lib=user32");
+                            println!("cargo:rustc-link-lib=crypt32");
+                        }
+                    } else {
+                        println!("cargo:rustc-link-lib=crypto");
+                        println!("cargo:rustc-link-lib=ssl");
+                    }
+                }
+            }
+        } else {
+            cfg.file("sqlite3/sqlite3.c");
+        }
+
         cfg.compile("libsqlite3.a");
     }
 }
