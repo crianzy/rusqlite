1d3e92644180a0e801068557f5b041b313f09253
chenzhiyong <chenzhiyong@bytedance.com>
2019-12-12 20:28:40 +0800

    ..

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -165,10 +165,10 @@ mod build {
 //            return HeaderLocation::FromEnvironment;
 //        }
 
-        let dir  = env::var(format!("{}_LIB_DIR", env_prefix())).unwrap();
-            println!("cargo:rustc-link-search={}", "/Users/chenzhiyong/Documents/bytedance/rocket/rust-sdk/deps/test_clink/android/armv7");
-            println!("cargo:rustc-link-lib={}", link_lib);
-            return HeaderLocation::FromEnvironment;
+//        let dir  = env::var(format!("{}_LIB_DIR", env_prefix())).unwrap();
+        println!("cargo:rustc-link-search={}", "/Users/chenzhiyong/Documents/bytedance/rocket/rust-sdk/deps/test_clink/android/armv7");
+        println!("cargo:rustc-link-lib={}", link_lib);
+        return HeaderLocation::FromEnvironment;
 
 //        if let Some(header) = try_vcpkg() {
 //            return header;
